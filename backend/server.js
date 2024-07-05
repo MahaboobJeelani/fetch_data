2f9ffbdacbb028ec98de8fb64a99e8c2e65e0da3
Test <mahaboobjeelani248@gmail.com>
2024-07-05 19:48:54 +0530

    Fetching data

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const fs = require('fs').promises;
+
+const fetchData = async () => {
+
+    try {
+        const [responseA, responseB] = await Promise.all([
+            axios.get(`https://jsonplaceholder.typicode.com/todos`),
+            axios.get(`https://fakestoreapi.com/products`)
+        ]);
+
+        const combinedData = {
+            dataA: responseA.data,
+            dataB: responseB.data
+        };
+
+        fs.writeFile('combinedData.json', JSON.stringify(combinedData, null, 2), 'utf-8');
+        console.log('Combined data written to combinedData.json');
+
+    } catch (error) {
+        console.error('Error fetching data from APIs or writing to file:', error.message);
+    }
+};
+
+fetchData();
